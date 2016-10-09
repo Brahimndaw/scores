518cf0049da2e46d6ea69a1667d17d31d4ace2f5
Lamine Ndaw <brahimndaw@gmail.com>
2016-10-09 19:16:25 -0400

    workingn on header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/scoreboard', to: 'home#index'
+  get '/scoreboard', to: 'home#scoreboard'
   get '/', to: 'home#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
